354148d9a7f5752518d48ce99a04a10fd30b7da1
Ali Ghezal <alighezal773@gmail.com>
2026-08-18 22:55:06 +0100

    Containerise Flask web app

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def history():
     return render_template("history.html", prices=rows, station=station)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
